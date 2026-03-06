4eb2f894a3419f3ef24eee88bd9e78a22c5959d5
ranjith1204 <ranjithkumarphy7@gmail.com>
2026-03-06 08:59:11 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Hello from Kubernetes</h1>
+        <p>Hostname: ${os.hostname()}</p>
+    `);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
